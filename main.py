2fc4bb713fa09efda600af8b34e0ccab4907912f
fadaishaitaiyang <63134541+fadaishaitaiyang@users.noreply.github.com>
2023-07-06 10:11:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,9 @@ if __name__ == "__main__":
     parser.add_argument('--test_annotation', type=str, default='./data/goo_dataset/goosynth/picklefile/goosynth_test_v2_no_segm.pkl')
     
     args=parser.parse_args()
-    #--------------------------------------------#
-    #0 gaze estimation + object detection
-    #1 object detection
-    # --------------------------------------------#
+    
     train_mode=args.train_mode
-    # -------------------------------#
-    #   Labeling path, image path
-    # -------------------------------#
+    
     train_dir = args.train_dir
     train_annotation = args.train_annotation
     test_dir = args.test_dir
@@ -45,35 +40,16 @@ if __name__ == "__main__":
             f=open('./data/logsSynth_1/performance_1.csv','a+')
             writer=csv.writer(f)
             writer.writerow(table_head)
-    # -------------------------------#
-    #   Whether to use Cuda
-    # -------------------------------#
     Cuda = True
-    # --------------------------------------------------------#
-    #   Classification of object detection
-    # --------------------------------------------------------#
     classes_path = './data/anchors/voc_classes.txt'
-    # ---------------------------------------------------------------------#
-    # anchors_path represents the txt file corresponding to the a priori box, which is generally not modified.
-    # anchors_mask is used to help the code find the corresponding a priori box, generally not modified.
-    # ---------------------------------------------------------------------#
     if train_mode==0:
         anchors_path = './data/anchors/yolo_anchors.txt'
         anchors_mask = [[0, 1, 2]]
     if train_mode==1:
         anchors_path = './data/anchors/yolo_anchors_3.txt'
         anchors_mask = [[6, 7, 8], [3, 4, 5], [0, 1, 2]]
-    # ----------------------------------------------------------------------------------------------------------------------------#
-    #   Resume train
-    # ----------------------------------------------------------------------------------------------------------------------------#
     model_path = ''
-    # ------------------------------------------------------#
-    #   The input shape size must be a multiple of 32
-    # ------------------------------------------------------#
     input_shape = [224, 224]
-    # ------------------------------------------------------#
-    #   Tricks application
-    # ------------------------------------------------------#
     Cosine_lr = True
     label_smoothing = 0
 
@@ -85,23 +61,11 @@ if __name__ == "__main__":
     UnFreeze_Epoch = 100
     Unfreeze_batch_size = 64
     Unfreeze_lr = 1e-4
-    # ------------------------------------------------------#
-    # Whether to freeze training, the default is to freeze the main training first and then unfreeze the training.
-    # ------------------------------------------------------#
     Freeze_Train = True
-    # ------------------------------------------------------#
-    # Used to set whether to use multiple threads to read data
-    # ------------------------------------------------------#
     num_workers = 8
-    # ----------------------------------------------------#
-    #   Get classes and anchors
-    # ----------------------------------------------------#
     class_names, num_classes = get_classes(classes_path)
     anchors, num_anchors = get_anchors(anchors_path)
 
-    # ------------------------------------------------------#
-    #   Create the model
-    # ------------------------------------------------------#
     model = GaTectorBody(anchors_mask, num_classes,train_mode)
 
     weights_init(model)
@@ -129,14 +93,6 @@ if __name__ == "__main__":
             os.mkdir('./data/logsReal/')
         loss_history=LossHistory("./data/logsReal/")
 
-    # ------------------------------------------------------#
-    # The main feature extraction network feature is general, freezing training can speed up the training speed
-    # It can also prevent the weight from being destroyed in the early stage of training.
-    # Init_Epoch is the initial generation
-    # Freeze_Epoch is the generation of freeze training
-    # UnFreeze_Epoch total training generation
-    # Prompt OOM or insufficient video memory, please reduce Batch_size
-    # ------------------------------------------------------#
     if True:
         batch_size = Freeze_batch_size
         lr = Freeze_lr
@@ -162,9 +118,6 @@ if __name__ == "__main__":
         if epoch_step == 0 or epoch_step_val == 0:
             raise ValueError("The data set is too small for training. Please expand the data set.")
 
-        # ------------------------------------#
-        #   Freeze a certain part of training
-        # ------------------------------------#
         if Freeze_Train:
             for param in model.backbone.parameters():
                 param.requires_grad = False
